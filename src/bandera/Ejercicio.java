2eef581dabcce459ce6479708c3cefa271ba1877
Matii <Matii@Mats-MacBook-Pro-2.local>
2017-09-13 00:46:31 -0300

    Se agrega matriz que almacena los retazos de cada escuela

--- src/bandera/Ejercicio.java
+++ src/bandera/Ejercicio.java
@@ -13,29 +13,40 @@ public class Ejercicio {
 	String miPathEntrada = "Bandera.in";
 	Scanner sc = new Scanner (new File(miPathEntrada));
 	Carretel carretel = new Carretel(sc.nextDouble());
-	Bandera [] escuelas = new Bandera [sc.nextInt()];
+	Bandera [] escuelas = new Bandera [sc.nextInt()]; 
 	double sobrante;
 	int cantMaxDeCosturas ;
-
+	int [][] matRetazos = new int [escuelas.length][5];
 	for (int i = 0; i < escuelas.length; i++) {
 		escuelas[i] = new Bandera(sc.nextDouble());
 		escuelas[i].alargarBandera(escuelas[i].getPrimerRetazo(), carretel);
 		carretel.cortarRetazo(escuelas[i].getPrimerRetazo());
 	}
-
+	
+	int j=0;
 	while(carretel.getLargo()>0 && carretel.puedeCortarDelCarretel(escuelas[0].sacarDigitoUltimoRetazo()+escuelas[0].getUltimoRetazo())) {
 	for (int i = 0; i < escuelas.length; i++) {
 		double largoACoser=escuelas[i].sacarDigitoUltimoRetazo()+escuelas[i].getUltimoRetazo();
 		if(carretel.cortarRetazo(largoACoser))
 				escuelas[i].alargarBandera(largoACoser,carretel);
+				matRetazos[i][j]=(int)escuelas[i].sacarDigitoUltimoRetazo();
 		}
-	
+	j++;
 	}
-	
+	sc.close();	
+
+	for (int i=0;i<escuelas.length;i++){
+			for (int a=0;a<4;a++) {
+					System.out.print(matRetazos[i][a]+"\t");
+			}
+		System.out.println(" ");
+	}
+
 	sobrante=carretel.getLargo();
 	cantMaxDeCosturas=buscarCosturasMaximas (escuelas);
-	sc.close();	
-	}
+	System.out.println("Sobrante del carretel:"+sobrante);
+	System.out.println("Cantidad de costuras Maximas:"+cantMaxDeCosturas);
+}
 	
 	public static int buscarCosturasMaximas (Bandera [] escuelas) {
 		int maximo=escuelas[0].getCosturas();
